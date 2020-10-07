e1ba09fee21bbd31a03beb7a89a1df888c20bc29
alekszych <aleks.zych@icloud.com>
2020-10-07 22:32:46 +0200

    added login

--- Users/urls.py
+++ Users/urls.py
@@ -1,8 +1,8 @@
-
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('register/', views.register, name="register"),
-    path('register/verification/', views.verification, name="verification"),
+    path('register/verify/', views.verification, name="verification"),
+    path('login/', views.login, name="login"),
 ]
--- Users/views.py
+++ Users/views.py
@@ -1,7 +1,8 @@
 import os
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 import requests
+import json
 
 
 # Create your views here.
@@ -25,20 +26,43 @@ def register(request):
             if req.status_code == 200:
                 request.session['email_session'] = email
                 return redirect(verification)
-
     return render(request, 'registration_page.html')
 
 
 def verification(request):
     email = str(request.session.get('email_session'))
     code = str(request.POST.get('code'))
-    if email and code is not None:
+    if email is not None and code is not None:
         url = os.environ.get("URL", 'https://api.giftshare.xyz/login/verifyaccount/' + email + "/" + code)
         url = "%s" % url
         req = requests.post(url)
         print("email:" + email)
+        print(req.content)
         if req.status_code == 200:
-            return HttpResponse(req.content)
+            return redirect('login')
     return render(request, 'verification_page.html')
 
 
+def login(request):
+    if (request.session.get('email_session')) is not None:
+        email = str(request.session.get('email_session'))
+    else:
+        email = str(request.POST.get('email'))
+    print(email)
+    password = str(request.POST.get('password'))
+    if email and password is not None:
+        url = os.environ.get("URL", 'https://api.giftshare.xyz/login/signin')
+        url = "%s" % url
+        body = {
+            "email": email,
+            "password": password,
+        }
+        req = requests.post(url, json=body)
+        print("status code logowania: " + str(req.status_code))
+        response = req.json()
+        request.session['token_session'] = response.get('token')
+        if req.status_code == 200:
+            return HttpResponse("zalogowano")
+    return render(request, 'login_page.html', {
+        "email": email,
+    })
